11f94a63a942291303fc0bd74a76f90e6a20eea5
Akaneno Ekong <mac@MACs-MacBook-Pro.local>
2026-05-14 11:11:38 +0100

    Initial commit

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,40 +1,47 @@
 const services = [
-    {
-        title: "Social media marketing",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti qui soluta cupiditate",
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-        </svg>`,
-    },
-    {
-        title: "Amazon affiliate marketing",
-        description: "Sunt, ipsam, necessitatibus sint fugit officia laboriosam minima ab ullam at magni et. Quaerat, sint!",
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-        </svg>`,
-    },
-    {
-        title: "Email marketing",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti qui soluta cupiditate",
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-        </svg>`,
-    },
-    {
-        title: "Social media marketing",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti qui soluta cupiditate",
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-        </svg>`,
-    },
-    {
-        title: "Amazon affiliate marketing",
-        description: "Sunt, ipsam, necessitatibus sint fugit officia laboriosam minima ab ullam at magni et. Quaerat, sint!",
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-        </svg>`,
-    },
-    {
-        title: "Email marketing",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti qui soluta cupiditate",
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-        </svg>`,
-    },
+  {
+    week: "Week 1",
+    title: `M365 Fundamentals`,
+    description:
+      "Foundations of M365 Administration, Tenant Setup & Configuration, User & Identity Management, Roles & Access Control, License & Subscription Management, Security & Compliance Fundamentals, Monitoring & Reporting, Service Health & Incident Management, Automation & PowerShell Basics",
+    icon: `<img src="../images/teaser-4.jpg" width="100" height=100>`,
+  },
+  {
+    week: "Week 2",
+    title: "Intro to Microsoft Exchange",
+    description:
+      "EAC, Mailflow Rules, User Mailbox and Receipient Management, Security and Protection basics, Email authentication (SPF, DMARC, DKIM), Mailbox Policies and Compliance, Exchange Powershell Scripting",
+    icon: `<img src="../images/teaser-2.jpg" width=100 height=100>`,
+  },
+  {
+    week: "Week 3",
+    title: "Intro to SharePoint & OneDrive",
+    description:
+      "SharePoint Sites & Permissions, Document Management & Governance, SharePoint/OneDrive Administration & Sharing, Security, Compliance & Sync, Automation + Real-world Scenarios",
+    icon: `<img src="../images/teaser-3.jpg" width=100 height=100>`,
+  },
+
+  {
+    week: "Week 4",
+    title: "Intro to Microsoft Entra ID",
+    description:
+      "Microsoft Entra ID overview, Tenant Setup & Configuration, Users & Group Management, Authentication Methods & MFA, Conditional Access Policies, Identity Protection & Security, Application Registration & Enterprise Apps, Device Management Integration, Roles & Administrative Units, Monitoring, Auditing & Sign-In Logs, Automation & PowerShell Basics",
+    icon: `<img src="../images/teaser.jpg" width=100 height=100>`,
+  },
+  {
+    week: "Week 5",
+    title: "Intro to Microsoft Power Apps",
+    description:
+      "Foundations of Power Apps, Environment Setup & Management, Canvas App Development, Model-Driven Apps Basics, Data Sources & Integration, Dataverse Fundamentals, User Interface & Experience Design, Business Logic & Formulas, Security & Permissions, App Deployment & Sharing, Monitoring & Analytics, Automation with Power Automate",
+    icon: `<img src="../images/powerplatform.png" width=100 height=100>`,
+  },
+  {
+    week: "Week 6",
+    title: "Intro to M365 Copilot",
+    description:
+      "Foundations of Copilot in Microsoft 365, Setup & Licensing, Data Access & Permissions, Security & Compliance, Prompting Techniques & Best Practices, Copilot in Outlook, Teams, Word, Excel & PowerPoint,Monitoring, Reporting & Adoption",
+    icon: `<img src="../images/teaser-5.jpg" width=100 height=100>`,
+  },
 ];
 
 export { services };
