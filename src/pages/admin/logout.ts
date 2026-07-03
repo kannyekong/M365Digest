d19ec0ce6cb4862bacd55e7cc602ea827fedd309
Akaneno Ekong <mac@MACs-MacBook-Pro.local>
2026-07-03 10:40:43 +0100

    modified key site features

--- src/pages/admin/logout.ts
+++ src/pages/admin/logout.ts
@@ -2,6 +2,8 @@ import type { APIRoute } from "astro";
 
 import { logout } from "../../lib/auth";
 
+export const prerender = false;
+
 export const GET: APIRoute = async () => {
   await logout();
 
