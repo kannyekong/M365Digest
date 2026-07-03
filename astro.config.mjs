7d361b01deaf1415074cd8004db6267b009b8089
Akaneno Ekong <mac@MACs-MacBook-Pro.local>
2026-07-03 10:48:44 +0100

    modified key site features

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import vercel from "@astrojs/vercel";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  output: "hybrid",
+  output: "static",
 
   adapter: vercel(),
 
